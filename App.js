5f23e0463a2d0652ef5be5838d1de7aa778cc428
Sajjad <sajjad@Sajjads-MacBook-Air.local>
2021-04-13 17:15:07 +0500

    layout

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button ,TouchableOpacity,Alert,Modal} from 'react-native';
+import { StyleSheet, Text, View, Button ,TouchableOpacity,Alert,Modal, Pressable} from 'react-native';
 
 
 
@@ -8,11 +8,9 @@ export default function App() {
   
     
   
-    const [getmodel,setmodel]=useState(false);
+    const [turn,setturn]=useState("player1");
    
-    function buttonClick(txt){
-
-    }
+    
   
   
     
@@ -21,36 +19,7 @@ export default function App() {
     
     
     <View style={styles.container}>
-        <Modal
-      animationType="slide"
-          
-          visible={getmodel}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            
-          }}
-      ><View style={{
-    justifyContent: "center",
-  
-    }}>
-    <View style={{ margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5}}>
-       
-      </View>
-      </View>
-        </Modal>
-       
+        
         
         <Header game_title="Tic Tac Toe" />
         <View style={{ flexDirection: 'row',}}>
@@ -72,7 +41,7 @@ export default function App() {
       <View style={{ flexDirection: 'row',}}>
         
         <View style={{ width: 100 }}>
-        <TouchableOpacity
+        <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
@@ -83,148 +52,151 @@ export default function App() {
         borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
       <View style={{ width: 100 }}>
-        <TouchableOpacity
+      <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
-        height:80,
         borderColor:'#FF0000',
-        borderWidth:2,
+        height:80,
         borderTopWidth:0,
+        borderLeftWidth:0,
+        borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
       <View style={{ width: 100 }}>
-        <TouchableOpacity
+      <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
         borderColor:'#FF0000',
-        borderWidth:2,
-        borderTopWidth:0,
         height:80,
+        borderTopWidth:0,
+        borderLeftWidth:0,
         borderRightWidth:0,
+        borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
         
         </View>
         <View style={{ flexDirection: 'row',}}>
         
         <View style={{ width: 100 }}>
-        <TouchableOpacity
+        <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
-        height:80,
         borderColor:'#FF0000',
-        borderWidth:2,
+        height:80,
+        borderTopWidth:0,
         borderLeftWidth:0,
-        padding: 10}}
-        
-        
-      >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
-      </View>
-      <View style={{ width: 100 }}>
-        <TouchableOpacity
-        style={{alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        color:'#000000',
-        height:80,
-        borderColor:'#FF0000',
         borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
       <View style={{ width: 100 }}>
-        <TouchableOpacity
+      <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
         borderColor:'#FF0000',
-        borderWidth:2,
         height:80,
+        borderTopWidth:0,
+        borderLeftWidth:0,
+        borderWidth:2,
+        padding: 10}}
+        
+      >
+        
+      </Pressable>
+      </View>
+      <View style={{ width: 100 }}>
+      <Pressable
+        style={{alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        color:'#000000',
+        borderColor:'#FF0000',
+        height:80,
+        borderTopWidth:0,
+        borderLeftWidth:0,
         borderRightWidth:0,
+        borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
         
         </View>
         <View style={{ flexDirection: 'row',}}>
         
         <View style={{ width: 100 }}>
-        <TouchableOpacity
+        <Pressable
         style={{alignItems: "center",
         backgroundColor: "#DDDDDD",
         color:'#000000',
         borderColor:'#FF0000',
-        borderWidth:2,
         height:80,
-        borderBottomWidth:0,
+        borderTopWidth:0,
         borderLeftWidth:0,
-        padding: 10}}
-        
-        
-      >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
-      </View>
-      <View style={{ width: 100 }}>
-        <TouchableOpacity
-        style={{alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        color:'#000000',
-        borderColor:'#FF0000',
-        borderWidth:2,
-        height:80,
         borderBottomWidth:0,
+        borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
       <View style={{ width: 100 }}>
-        <TouchableOpacity
+      <Pressable
         style={{alignItems: "center",
-        height:80,
         backgroundColor: "#DDDDDD",
         color:'#000000',
         borderColor:'#FF0000',
+        height:80,
+        borderTopWidth:0,
+        borderLeftWidth:0,
+        borderBottomWidth:0,
         borderWidth:2,
+        padding: 10}}
+        
+      >
+        
+      </Pressable>
+      </View>
+      <View style={{ width: 100 }}>
+      <Pressable
+        style={{alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        color:'#000000',
+        borderColor:'#FF0000',
+        height:80,
+        borderTopWidth:0,
+        borderLeftWidth:0,
         borderBottomWidth:0,
         borderRightWidth:0,
+        borderWidth:2,
         padding: 10}}
         
-        
       >
-        <Text style={{color:"black"}}></Text>
-      </TouchableOpacity>
+        
+      </Pressable>
       </View>
         
         </View>
